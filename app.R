f5d3cb448fcaaf865cbe8eb3245e0d5a6d1aac4b
Edward <securedbread@protonmail.com>
2024-05-10 15:21:55 -0700

    Added Sidebar

--- app.R
+++ app.R
@@ -4,20 +4,34 @@ library(httr)
 library(jsonlite)
 library(lubridate)
 
-ui <- fluidPage(
-  div(class = "intro",
-    titlePanel("Dashboard - Weather Reports"),
-    h4("Powered by ", 
-       actionLink("website", "edwardcreates.ca", onclick = "window.open('https://www.edwardcreates.ca/', '_blank')")),
-    
+ui <- fluidPage(div(
+  class = "intro",
+  titlePanel("Dashboard - Weather Reports"),
+  h4(
+    "Powered by ",
+    actionLink("website", "edwardcreates.ca", onclick = "window.open('https://www.edwardcreates.ca/', '_blank')")
   ),
-  actionButton("refresh", "Refresh", 
-               icon = icon("arrows-rotate")),
-  card(
-    div(class = "k-hstack", 
+  
+),
+sidebarLayout(
+  sidebarPanel(
+    card(
+      actionButton("van", "Vancouver", icon = icon("building")),
+      actionButton("bur", "Burnaby", icon = icon("building")),
+      actionButton("rich", "Richmond", icon = icon("building")),
+      actionButton("sur", "Surrey", icon = icon("building")),
+      actionButton("lang", "Langley", icon = icon("building")),
+      actionButton("abby", "Abbotsford", icon = icon("building")),
+    )
+  ),
+  mainPanel(
+    actionButton("refresh", "Refresh", icon = icon("arrows-rotate")),
+    card(
+      div(
+        class = "k-hstack",
         layout_columns(
           value_box(
-            title = "Current Temp",
+            title = "Current Temp - Comming Soon...",
             value = "XX ˚C",
             showcase = bsicons::bs_icon("calendar-event"),
             theme = value_box_theme(bg = "#e6f2fd", fg = "#0B538E"),
@@ -47,10 +61,10 @@ ui <- fluidPage(
             theme = value_box_theme(bg = "#e6f2fd", fg = "#0B538E"),
             class = "value_box"
           ),
-        ), 
-    ),
-    tags$style(
-      "
+        ),
+      ),
+      tags$style(
+        "
       .k-hstack {
         margin-top: 1rem;
         margin-bottom: 3rem;
@@ -62,16 +76,14 @@ ui <- fluidPage(
         width: 4/6;
       }
       "
+      ),
     ),
-  ),
-  card(
     
-    plotOutput("apiPlot")
-  ),
-  card(
-    plotOutput("statPlot")
+    card(plotOutput("apiPlot")),
+    card(plotOutput("statPlot")),
+    card(plotOutput("boxPlot"))
   )
-)
+))
 
 server <- function(input, output) {
   observeEvent(input$refresh, {
@@ -81,21 +93,26 @@ server <- function(input, output) {
   
   # Must pull weather data from Vancouver
   fetchData <- function() {
-    response <- GET("https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&hourly=temperature_2m&timeformat=unixtime&timezone=America%2FLos_Angeles&forecast_days=1")
+    response <- GET(
+      "https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&hourly=temperature_2m&timeformat=unixtime&timezone=America%2FLos_Angeles&forecast_days=1"
+    )
     
-    if (status_code(response) >= 200 && status_code(response) < 400) {
+    if (status_code(response) >= 200 &&
+        status_code(response) < 400) {
       data <- fromJSON(content(response, "text"))
       return(list(
-        time = data$hourly$time, 
+        time = data$hourly$time,
         temp = data$hourly$temperature_2m
       ))
-    } 
+    }
     
-    if (status_code(response) >= 400 && status_code(response) < 500) {
+    if (status_code(response) >= 400 &&
+        status_code(response) < 500) {
       return("API Failed due to Human Error")
     }
     
-    if (status_code(response) >= 500 && status_code(response) < 600) {
+    if (status_code(response) >= 500 &&
+        status_code(response) < 600) {
       return("API Failed due to Server Side Error\n\nPlease review the server...")
     }
   }
@@ -132,35 +149,85 @@ server <- function(input, output) {
     # Isolates the Time from the Datetime
     unconverted_hours <- format(unconverted, "%H:%M")
     
-    listOfHours <- c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00")
-    xLimits <- 1:length(listOfHours)
-    plot(xLimits, data$temp, type = "b", col = "#0B538E", xlab = "Hours in PST", ylab = "Temperature (°C)", main = chartTitle,
-         xlim = range(xLimits)
+    listOfHours <- c(
+      "00:00",
+      "01:00",
+      "02:00",
+      "03:00",
+      "04:00",
+      "05:00",
+      "06:00",
+      "07:00",
+      "08:00",
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00"
     )
-    #identify(xLimits, data$temp, labels = data$temp, n = length(data$temp), offset = 0.5)
-    text(xLimits, data$temp, labels = as.character(data$temp), pos = 1, offset = 1.1)
+    xLimits <- 1:length(listOfHours)
+    plot(
+      xLimits,
+      data$temp,
+      type = "b",
+      col = "#0B538E",
+      xlab = "Hours in PST",
+      ylab = "Temperature (°C)",
+      main = chartTitle,
+      xlim = range(xLimits)
+    )
+    text(
+      xLimits,
+      data$temp,
+      labels = as.character(data$temp),
+      pos = 1,
+      offset = 1.1
+    )
+  })
+  
+  # Box Plot of the Temp Data
+  output$boxPlot <- renderPlot({
+    data <- fetchData()
+    boxplot(data$temp, main = "Boxplot of Todays Changing Weather", col =
+              "#e6f2fd")
   })
   
   # Bar Chart of general Stats
   output$statPlot <- renderPlot({
     data <- fetchData()
     chartTitle = "Comparison of General Stats about Hourly Temps"
-    yaxis = c(min(data$temp, na.rm = TRUE), mean(data$temp, na.rm = TRUE), IQR(data$temp, na.rm = TRUE), max(data$temp, na.rm = TRUE))
+    yaxis = c(
+      min(data$temp, na.rm = TRUE),
+      mean(data$temp, na.rm = TRUE),
+      IQR(data$temp, na.rm = TRUE),
+      max(data$temp, na.rm = TRUE)
+    )
     xLabels = c("Min Temp", "Average Temp", "IQR", "Max Temp")
     
-    barplot(yaxis, 
-            xlab = "Stats",
-            ylab = "Temperature ˚C",
-            names.arg = xLabels,
-            col = "#e6f2fd",
-            main = chartTitle
+    barplot(
+      yaxis,
+      xlab = "Stats",
+      ylab = "Temperature ˚C",
+      names.arg = xLabels,
+      col = "#e6f2fd",
+      main = chartTitle
     )
-    text(x = xLabels, 
-          
-         labels = yaxis,
-         names.arg = xLabels,
-         pos = 3,
-         offset = yaxis + 1)
+    text(
+      x = xLabels,
+      labels = yaxis,
+      pos = 3,
+      offset = yaxis + 1
+    )
   })
 }
 
